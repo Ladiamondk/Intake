a776d4ef5a805ec879bccf749ecd0740f046ea6c
LaDiamond Kidd <ladiamondk@yahoo.com>
2023-12-06 11:17:13 -0800

    added the progress bar check box transitionso when a  client select next the text turns to a checkmark

--- js/script.js
+++ js/script.js
@@ -6,12 +6,34 @@ const agreements = document.getElementById('agreements')
 const dgnext = document.getElementById('dgNext')
 const mdprev =document.getElementById('mdPrev')
 
+/*This area is for operating the next and previous buttons*/
+function next(current, next){
+    current.style.display = 'none';
+    next.style.display = 'contents'; 
+}
+function prev(current, prev){
+    current.style.display = 'none';
+    prev.style.display = 'flex';
+}
+/*demo and medical */
+const checkmark = new Image();
+checkmark.src = 'resources/checkmark.png';
+checkmark.width = 20;
+checkmark.height = 20;
+checkmark.alt = 'green checkmark';
+const pDemo = document.getElementById('p-1');
 
-dgnext.addEventListener('click', () => {
-    demo.style.display = 'none';
-    medical.style.display = 'contents';
-})
+dgnext.addEventListener('click', () =>{ 
+    next(demo, medical);
+    pDemo.innerText = '';
+    pDemo.appendChild(checkmark);
+});
+
 mdprev.addEventListener('click', () =>{
-    medical.style.display = 'none';
-    demo.style.display = 'flex';
-})
+    prev(medical, demo);
+    pDemo.innerText = 'Demographics';
+});
+
+/**This area is for the progress bar */
+
+
