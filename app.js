eefe8c6a3a7c0c8cf21bb0f1e28f4198fa4b1da5
LaDiamond Kidd <ladiamondk@yahoo.com>
2023-12-28 16:24:37 -0800

    and an input element for each question that has an other input value.

--- app.js
+++ app.js
@@ -24,11 +24,83 @@ app.get('/', (req,res) => {
 //handle form submission
 app.post('/registration', async(req, res) => {
     try {
-        const {dg1, dg2} = req.body;
+        const {
+            todays_date,
+            roots_email,
+            roots_site,
+            visit_purpose,
+            first_name,
+            last_name,
+            middle_name,
+            preferred_name,
+            birth_date,
+            ssn,
+            sex,
+            gender_identity,
+            pronouns,
+            sexual_orientation,
+            personal_email,
+            home_phone,
+            cell_phone,
+            address,
+            preferred_contact_method,
+            emergency_contact_name1,
+            emergency_contact_relationship1,
+            emergency_contact_number1,
+            emergency_contact_name2,
+            emergency_contact_relationship2,
+            emergency_contact_number2,
+            children_name_age,
+            child_development_concern,
+            last_12months_child_pcp_visit,
+            pediatric_care_interest,
+            pharmacy,
+            identified_race,
+            identified_ethnicity,
+            languages_spoken,
+            martial_status,
+            veterans_status
+        } = req.body;
 
-        const result = await pool.query('INSERT INTO patient (todaydate, rootsemail) VALUES ($1, $2)', [dg1, dg2]);
+        const result = await pool.query('INSERT INTO demographics (todays_date, roots_email, roots_site, visit_purpose, first_name, last_name, middle_name, preferred_name, birth_date, ssn, sex, gender_identity, pronouns, sexual_orientation, personal_email, home_phone, cell_phone, address, preferred_contact_method, emergency_contact_name1, emergency_contact_relationship1, emergency_contact_number1, emergency_contact_name2, emergency_contact_relationship2, emergency_contact_number2, children_name_age, child_development_concern, last_12months_child_pcp_visit, pediatric_care_interest, pharmacy, identified_race, identified_ethnicity, languages_spoken, martial_status, veterans_status) VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13,$14,$15,$16,$17,$18,$19,$20,$21,$22,$23,$24,$25,$26,$27,$28,$29,$30,$31,$32,$33,$34,$35)', [
+            todays_date,
+            roots_email,
+            roots_site,
+            visit_purpose,
+            first_name,
+            last_name,
+            middle_name,
+            preferred_name,
+            birth_date,
+            ssn,
+            sex, 
+            gender_identity,
+            pronouns,
+            sexual_orientation,
+            personal_email,
+            home_phone,
+            cell_phone,
+            address,
+            preferred_contact_method,
+            emergency_contact_name1,
+            emergency_contact_relationship1,
+            emergency_contact_number1,
+            emergency_contact_name2,
+            emergency_contact_relationship2,
+            emergency_contact_number2,
+            children_name_age,
+            child_development_concern,
+            last_12months_child_pcp_visit,
+            pediatric_care_interest,
+            pharmacy,
+            identified_race,
+            identified_ethnicity,
+            languages_spoken,
+            martial_status,
+            veterans_status       
+        ]);
 
-        console.log(result.rows[0]);
+        console.log(result.id);
 
         res.send('Form submitted successfully!');
     } catch(error){
--- js/script.js
+++ js/script.js
@@ -121,7 +121,295 @@ amprev.addEventListener('click', () => {
     pScreen.innerText = 'Screening';
 })
 //Demographics section of the form
-//Additional Emergency contact
+//creates a text input element if other is selected
+        //rootg_site question
+const roots_site = document.getElementById('rootsSite');
+//specifies the question that is targeted
+if(roots_site){
+    //stores the value of the selected button
+    let site;
+    //this will store the values of the input element created when 'other' is selected
+    let input;
+    //this is to query all the input elements with the type 'radio' with in the root_site fieldset
+    const rootsSiteRadios = roots_site.querySelectorAll('input[type="radio"]');
+    //great an event listener on the roots_site fieldset
+    roots_site.addEventListener('click', () => {
+        //when clicked it looks through the rootsSiteRadios
+        for(let i = 0; i < rootsSiteRadios.length; i++){
+            //if statement checks for a checked psuedo code
+            if(rootsSiteRadios[i].checked){
+                //store that value in the site variable
+                site = rootsSiteRadios[i].value;
+                //check if site is store the value of 'other
+                if(site == 'other'){
+                    //if returns true, check to insure that the input variable is empty/ return false
+                    if(!input){
+                        //if return false, create the input elment and assign to the input variable
+                        //creates a input element
+                        input = document.createElement('input');
+                        //assigns values to attributes
+                        input.name = 'roots_site';
+                        input.value = '';
+                        input.placeholder = 'Please specify';
+                        //append to the element you want it to displayed
+                        roots_site.appendChild(input)
+                    }
+                    //this insures that the input element is displayed ever time 'other' is selected
+                    input.style.display = 'inline';
+                } else {
+                    //checks if input returns true
+                    if(input){
+                        input.style.display = 'none'
+                        input.name = '';
+                        input.value = '';
+                    }
+                }
+            }
+        }
+    })
+}
+        //gender_identity question
+const gender_identity = document.getElementById('genderIdentity');
+if(gender_identity){
+    let genderIdentified;
+    let input;
+    const genderIdentifiedRadios = gender_identity.querySelectorAll('input[type="radio"]');
+
+    gender_identity.addEventListener('click', () =>{
+        for(let i = 0; i < genderIdentifiedRadios.length; i++){
+            if(genderIdentifiedRadios[i].checked){
+                genderIdentified = genderIdentifiedRadios[i].value;
+
+                if(genderIdentified === 'other'){
+                    gender_identity_other.style.display = 'inline';
+
+                    if(!input){
+                        //createing new element
+                        input = document.createElement('input');
+                        input.name = 'gender_identity';
+                        input.value = '';
+                        input.placeholder = 'Please specify';
+                        //append to element
+                        gender_identity.appendChild(input); 
+                    }
+                }else{
+                    gender_identity_other.style.display = 'inline';
+                    if(input){
+                       input.style.display = 'none';
+                       input.value = '';
+                       input.name = '';
+                    }
+                }   
+            }
+        }
+    })
+}
+        //pronouns question
+const pronouns = document.getElementById('pronouns');
+if(pronouns){
+    let preferredPronouns;
+    let input;
+
+    const pronounsRadios = pronouns.querySelectorAll('input[type="radio"]');
+    pronouns.addEventListener('click',() =>{
+        for(let i = 0; i < pronounsRadios.length; i++){
+            if(pronounsRadios[i].checked){
+                preferredPronouns = pronounsRadios[i].value;
+                if(preferredPronouns == 'other'){
+                    if(!input){
+                        input = document.createElement('input');
+                        input.name = 'pronouns';
+                        input.value = '';
+                        input.placeholder = 'Please specify';
+
+                        pronouns.appendChild(input);
+                    }
+                    input.style.display = 'inline';
+                } else {
+                    if(input){
+                        input.name = '';
+                        input.value = '';
+                        input.style.display = 'none';
+                    }
+                }
+            }
+        }
+    })
+}
+        //sexual_orientation question
+const sexual_orientation = document.getElementById('sexual_orientation');
+if(sexual_orientation){
+    let other_sexual_orientation;
+    let input;
+
+    const sexual_orientationRadios = sexual_orientation.querySelectorAll('input[type="radio"]');
+    sexual_orientation.addEventListener('click', () => {
+        for(let i = 0; i < sexual_orientationRadios.length; i++){
+            if(sexual_orientationRadios[i].checked){
+                other_sexual_orientation = sexual_orientationRadios[i].value;
+                if(other_sexual_orientation == 'other'){
+                    if(!input){
+                        input = document.createElement('input');
+                        input.name = 'sexual_orientation';
+                        input.value = '';
+                        input.placeholder = 'Please specify';
+
+                        sexual_orientation.appendChild(input)
+                    }
+                    input.style.display = 'inline';
+                } else {
+                    if(input){
+                        input.style.display = 'none';
+                        input.value = '';
+                        input.name = '';
+                    }
+                }
+            }
+        }
+    })
+}
+        //preferredContactMethod question
+const preferredContactMethod = document.getElementById('preferredContactMethod');
+if(preferredContactMethod){
+    let OtherpreferredContactMethod;
+    let input;
+
+    const preferredContactMethodRadios = preferredContactMethod.querySelectorAll('input[type="radio"]');
+    preferredContactMethod.addEventListener('click', () => {
+        for(let i = 0; preferredContactMethodRadios.length; i++){
+            if(preferredContactMethodRadios[i].checked){
+                OtherpreferredContactMethod = preferredContactMethodRadios[i].value;
+                if(OtherpreferredContactMethod == 'other'){
+                    if(!input){
+                        input = document.createElement('input');
+                        input.name = 'preferred_contact_method';
+                        input.value = '';
+                        input.placeholder = 'Please specify';
+
+                        preferredContactMethod.appendChild(input);
+                    }
+                    input.style.display = 'inline';
+                } else {
+                    if(input){
+                        input.value = '';
+                        input.name = '';
+                        input.style.display = 'none';
+                    }
+                }
+            }
+        }
+    })
+}
+        //identifiedRace question
+const identifiedRace = document.getElementById('identifiedRace');
+if(identifiedRace){
+    let input;
+    identifiedRace.addEventListener('click', () => {
+        const identified_race_other = document.getElementById('identified_race-other');
+        if(identified_race_other.checked){
+            if(!input){
+                input = document.createElement('input')
+                input.name = 'identified_race';
+                input.value = '';
+                input.placeholder = 'Please specify';
+
+                identifiedRace.appendChild(input)
+            }
+            input.style.display = 'inline';
+        } else {
+            input.style.display = 'none';
+            input.name = '';
+        }
+    })
+}
+        //identifiedEthnicity question
+const identifiedEthnicity = document.getElementById('identifiedEthnicity');
+if(identifiedEthnicity){
+    let input;
+    let OtherIdentifiedEthnicity;
+
+    const identifiedEthnicityRadios = identifiedEthnicity.querySelectorAll('input[type="radio"]');
+    identifiedEthnicity.addEventListener('click', () => {
+        for(let i = 0; i < identifiedEthnicityRadios.length; i++){
+            if(identifiedEthnicityRadios[i].checked){
+                OtherIdentifiedEthnicity = identifiedEthnicityRadios[i].value;
+                if(OtherIdentifiedEthnicity== 'other'){
+                    if(!input){
+                        input = document.createElement('input');
+                        input.name = 'identified_ethnicity';
+                        input.value = '';
+                        input.placeholder = 'Please specify';
+
+                        identifiedEthnicity.appendChild(input);
+                    }
+                    input.style.display = 'inline';
+                } else {
+                    if(input){
+                        input.style.display = 'none';
+                        input.value = '';
+                        input.name = '';
+                    }
+                }
+            }
+        }
+    })
+}
+        //languagesSpoken question
+const languagesSpoken = document.getElementById('languagesSpoken');
+if(languagesSpoken){
+    let input;
+    const languages_spoken_other = document.getElementById('languages_spoken-other');
+    languagesSpoken.addEventListener('click', () => {
+        if(languages_spoken_other.checked){
+            if(!input){
+                input = document.createElement('input');
+                input.name = 'languages_spoken';
+                input.value = '';
+                input.placeholder = 'Please specify';
+
+                languagesSpoken.appendChild(input);
+            }
+            input.style.display = 'inline';
+        } else {
+            if(input){
+                input.style.display = 'none';
+                input.value = '';
+            }
+        }
+    })
+}
+        // martialStatus question
+const martialStatus = document.getElementById('martialStatus');
+if(martialStatus){
+    let input;
+    let martialStatusOther;
+
+    const martialStatusRadios = martialStatus.querySelectorAll('input[type="radio"]');
+    martialStatus.addEventListener('click', () => {
+        for(let i = 0; i < martialStatusRadios.length; i++){
+            if(martialStatusRadios[i].checked){
+                martialStatusOther = martialStatusRadios[i].value;
+                if(martialStatusOther == 'other'){
+                    if(!input){
+                        input = document.createElement('input');
+                        input.name = 'martial_status';
+                        input.value = '';
+                        input.placeholder = 'Please specify';
+
+                        martialStatus.appendChild(input);
+                    }
+                    input.style.display = 'inline'
+                } else {
+                    if(input){
+                        input.style.display = 'none';
+                        input.name = '';
+                        input.value = '';
+                    }
+                }
+            }
+        }
+    })
+}
 function additionEC(){
     const addEBtn = document.getElementById('addEBtn');
     const addEContact = document.getElementById('dg23-24-25');
