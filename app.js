b62eb5d2050d603b512c522f7a84fc538cd44668
LaDiamond Kidd <ladiamondk@yahoo.com>
2024-01-09 18:11:42 -0800

    finshing touches

--- app.js
+++ app.js
@@ -191,18 +191,21 @@ app.post('/registration', async(req, res) => {
             receivingNavigationServices,
             agreementsTargetCaseManagement,
             agreementsHealthyMeasures,
-            financial_security,
-            employment_education,
-            mobility_communication,
-            healthcare,
-            social_supports,
-            legat_satus,
-            mental_health,
-            substance_use,
-            food_access
+            householdchildren0_5,
+            householdchildren6_17,
+            householdadults18_65,
+            householdadults65_plus,
+            TANF,
+            SSI,
+            GA,
+            SNAP,
+            WIC,
+            unemployment,
+            SDI,
+            rentalAssistance
         } = req.body;
 
-        const demographics = await pool.query('INSERT INTO demographics (todays_date, roots_email, roots_site, visit_purpose, first_name, last_name, middle_name, preferred_name, birth_date, ssn, sex, gender_identity, pronouns, sexual_orientation, personal_email, home_phone, cell_phone, address, preferred_contact_method, emergency_contact_name1, emergency_contact_relationship1, emergency_contact_number1, emergency_contact_name2, emergency_contact_relationship2, emergency_contact_number2, children_name_age, child_development_concern, last_12months_child_pcp_visit, pediatric_care_interest, pharmacy, identified_race, identified_ethnicity, languages_spoken, martial_status, veterans_status) VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13,$14,$15,$16,$17,$18,$19,$20,$21,$22,$23,$24,$25,$26,$27,$28,$29,$30,$31,$32,$33,$34,$35)', [
+        const demographics = await pool.query('INSERT INTO demographics (todays_date, roots_email, roots_site, visit_purpose, first_name, last_name, middle_name, preferred_name, birth_date, ssn, sex, gender_identity, pronouns, sexual_orientation, personal_email, home_phone, cell_phone, address, preferred_contact_method, emergency_contact_name1, emergency_contact_relationship1, emergency_contact_number1, emergency_contact_name2, emergency_contact_relationship2, emergency_contact_number2, children_name_age, child_development_concern, last_12months_child_pcp_visit, pediatric_care_interest, pharmacy, identified_race, identified_ethnicity, languages_spoken, martial_status, veterans_status, householdchildren0_5, householdchildren6_17, householdadults18_65, householdadults65_plus) VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13,$14,$15,$16,$17,$18,$19,$20,$21,$22,$23,$24,$25,$26,$27,$28,$29,$30,$31,$32,$33,$34,$35,$36,$37,$38,$39)', [
             todays_date,
             roots_email,
             roots_site,
@@ -237,7 +240,11 @@ app.post('/registration', async(req, res) => {
             identified_ethnicity,
             languages_spoken,
             martial_status,
-            veterans_status,       
+            veterans_status,
+            householdchildren0_5,
+            householdchildren6_17,
+            householdadults18_65,
+            householdadults65_plus       
         ]);
         console.log(demographics);
 
@@ -278,7 +285,7 @@ app.post('/registration', async(req, res) => {
             miscarriages,
         ]);
         console.log(medicalHistory);
-        const preventative = await pool.query('INSERT INTO preventative (healthInsurancePlan, policyNumber, primaryDoctor, lastPrimaryVisit, primayDoctorNameLocation, regularDentist, lastDentistVisit, dentistNameLocation, doctorsTherapistsCounselors, doctorsTherapistsCounselorsNamesLocations, healthcareManagementSocialOrCaseWorker, caseOrSocialWorkerNameLocation, selfHealthEvaluation, pastMonthPhysicalHealthSelfEval, diagnosedMedicalConditions, preventativeScreeningTest, lastYearScreening, dailyModerateStrenuousExcercise, minuteModerateStrenuousExcercise, pastYearEmergencyRoomVisit, smokeCigarettesStatus, cigarettesSmokedPerDay, tobaccoNicotineProducts, lastSmoke, smokeFrequency) VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13,$14,$15,$16,$17,$18,$19,$20,$21,$22,$23,$24,$25)', [
+        const preventative = await pool.query('INSERT INTO preventative (healthInsurancePlan, policyNumber, primaryDoctor, lastPrimaryVisit, primayDoctorNameLocation, regularDentist, lastDentistVisit, dentistNameLocation, doctorsTherapistsCounselors, doctorsTherapistsCounselorsNamesLocations, healthcareManagementSocialOrCaseWorker, caseOrSocialWorkerNameLocation, selfHealthEvaluation, pastMonthPhysicalHealthSelfEval, diagnosedMedicalConditions, preventativeScreeningTest, lastYearScreening, dailyModerateStrenuousExcercise, minuteModerateStrenuousExcercise, pastYearEmergencyRoomVisit, smokeCigarettesStatus, cigarettesSmokedPerDay, tobaccoNicotineProducts, lastSmoke, smokeFrequency, calworks_benefits, social_security_disability, general_assistance, calfresh_benefits, women_infant_children_benefits, unemployment_benefits, state_disability_insurance_benefits, rental_assistance_benefits ) VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13,$14,$15,$16,$17,$18,$19,$20,$21,$22,$23,$24,$25,$26,$27,$28,$29,$30,$31,$32,$33)', [
             healthInsurancePlan,
             policyNumber,
             primaryDoctor,
@@ -304,6 +311,14 @@ app.post('/registration', async(req, res) => {
             tobaccoNicotineProducts,
             lastSmoke,
             smokeFrequency,
+            TANF,
+            SSI,
+            GA,
+            SNAP,
+            WIC,
+            unemployment,
+            SDI,
+            rentalAssistance
         ]);
         console.log(preventative);
 
@@ -390,6 +405,7 @@ app.post('/registration', async(req, res) => {
             agreementsHealthyMeasures
         ]);
         console.log(agreements);
+
         let housingscore;
         if ((housingSituation == 'I own a house or apartment' || housingSituation == 'I rent a house or apartment') && past3MonthsLackOfHousing == 'never'){
             housingscore = 'low';
@@ -399,21 +415,73 @@ app.post('/registration', async(req, res) => {
             housingscore = 'high';
         } else {
             housingscore = 'low';
+        };
+
+        let financialSecurityScore;
+        if(incomeCoverHouseholdExpenses == 'no' && (past3MonthsCutSkipMealSize == 'most days' || past3MonthsCutSkipMealSize == 'every day')){
+            financialSecurityScore = 'high';
+        }else if((past3MonthsDifficultiesPayingEssentialBills == 'one month' || past3MonthsDifficultiesPayingEssentialBills == 'two months') && past3MonthsCutSkipMealSize == 'several days'){
+            financialSecurityScore = 'medium';
+        }else{
+            financialSecurityScore = 'low';
+        };
+
+        let educationEmploymentFlag;
+        if(todaysWorkSituation == 'I am unemployed, but looking for work' || educationTrainingParticipating == 'no, but I would like to'){
+            educationEmploymentFlag = 'yes';
+        }else{
+            educationEmploymentFlag = 'no';
         }
-        console.log(past3MonthsLackOfHousing);
-        console.log(housingSituation);
-        console.log(housingscore);
-        const socialVitalSigns = await pool.query('INSERT INTO social_vital_signs (housing, financial_security, employment_education, mobility_communication, healthcare, social_supports, legat_satus, mental_health, substance_use, food_access) VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9,$10)',[
+        let mobilityCommunicationFlag;
+        if(ownPersonalPhone == 'no' || pastYearLackOfConvenientTransportation == 'every day' || pastYearLackOfConvenientTransportation == 'most days' || dependableSupportPerson == 'no'){
+            mobilityCommunicationFlag = 'yes';
+        }else{
+            mobilityCommunicationFlag = 'no';
+        };
+
+        let healthcarePreventativeFlag;
+        if(healthInsurancePlan == 'none' || primaryDoctor == 'no' || regularDentist == 'no'){
+            healthcarePreventativeFlag = 'yes';
+        }else{
+            healthcarePreventativeFlag = 'no';
+        };
+
+        let healthcareGeneralScore;
+        if(selfHealthEvaluation == 'poor' || pastYearEmergencyRoomVisit !== 'none' || pastMonthPhysicalHealthSelfEval == 'every day' || pastMonthPhysicalHealthSelfEval == 'more than half the days'){
+            healthcareGeneralScore = 'high';
+        }else if(selfHealthEvaluation == 'fair' || pastMonthPhysicalHealthSelfEval == 'some days'){
+            healthcareGeneralScore = 'medium';
+        }else{
+            healthcareGeneralScore = 'low';
+        }
+
+        let healthcareCardioScore;
+        if(diagnosedMedicalConditions == 'Congestive heart failure' || diagnosedMedicalConditions == 'Heart disease' || diagnosedMedicalConditions == 'High blood pressure' || dailyModerateStrenuousExcercise == '0 days' || minuteModerateStrenuousExcercise == '0 min' || smokeCigarettesStatus == 'yes'){
+            healthcareCardioScore = 'high';
+        }else if(diagnosedMedicalConditions !== 'Congestive heart failure' || diagnosedMedicalConditions !== 'Heart disease' || !diagnosedMedicalConditions == 'High blood pressure' || dailyModerateStrenuousExcercise == '1-2 days' || minuteModerateStrenuousExcercise == '0 min'|| minuteModerateStrenuousExcercise == '15 min'){
+            healthcareCardioScore = 'medium';
+        }else if(diagnosedMedicalConditions !== 'none' || dailyModerateStrenuousExcercise == '3-4 days' || dailyModerateStrenuousExcercise == '5-7 days' || minuteModerateStrenuousExcercise !== '0 min'|| minuteModerateStrenuousExcercise !== '15 min'){
+            healthcareCardioScore = 'low';
+        }
+
+        let foodAcccessScore;
+        if(past12MonthsWorriedAboutFoodRunningOut == 'often' || past12MonthsFoodRanOut == 'often'){
+            foodAcccessScore = 'high';
+        }else if(past12MonthsWorriedAboutFoodRunningOut == 'sometimes' || past12MonthsFoodRanOut == 'sometimes'){
+            foodAcccessScore = 'medium';
+        }else{
+            foodAcccessScore = 'low';
+        }
+        
+        const socialVitalSigns = await pool.query('INSERT INTO social_vital_signs (housing, financial_security, employment_education, mobility_communication, healthcare_preventative, healthcare_general, healthcare_cardio, food_access) VALUES ($1,$2,$3,$4,$5,$6,$7,$8)',[
             housingscore,
-            financial_security,
-            employment_education,
-            mobility_communication,
-            healthcare,
-            social_supports,
-            legat_satus,
-            mental_health,
-            substance_use,
-            food_access
+            financialSecurityScore,
+            educationEmploymentFlag,
+            mobilityCommunicationFlag,
+            healthcarePreventativeFlag,
+            healthcareGeneralScore,
+            healthcareCardioScore,
+            foodAcccessScore
         ])
         res.send('Form submitted successfully!');
 
